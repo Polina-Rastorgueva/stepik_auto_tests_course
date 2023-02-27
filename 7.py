131c8b89fc2a357f39b76191b96d971ad8ff2558
Polina-Rastorgueva <polina-rastorgueva@yandex.ru>
2023-02-27 18:12:26 +0300

    Сумма 2 чисел и выбор ответа в селекте

---
+++ 7.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import time
+import math
+
+try: 
+    link = "http://suninjuly.github.io/selects2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element(By.ID, "num1")
+    y_element = browser.find_element(By.ID, "num2")
+    x = x_element.text
+    y = y_element.text
+    z = str(str(int(x)+int(y)))
+
+    
+    select = Select(browser.find_element(By.TAG_NAME, "select"))
+    select.select_by_value(z)
+
+    button = browser.find_element(By.CSS_SELECTOR, '[class="btn btn-default"]')
+    button.click()
+    
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
